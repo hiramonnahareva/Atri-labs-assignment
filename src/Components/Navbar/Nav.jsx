481eab44de5e61608e3c080754c8536b01f298e6
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2022-12-22 00:31:07 +0600

    initial setup & navbar

---
+++ src/Components/Navbar/Nav.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Nav.css';
+const Nav = () => {
+    return (
+        <div className=''>
+           <div className='wrapper nav'>
+            <a href="" className='nav-brand w-nav-brand w--current'>
+                <img src=" https://assets.website-files.com/5fef5619b640934b33c2385e/6078ab3cfa1bca879adb93d1_Group%2070.svg" alt="" />
+                </a>
+                <nav className='nav-menu w-nav-menu'>
+                    <a href="#about" className='nav-link w-nav-link'>About</a>
+                    <a href="#services" className='nav-link w-nav-link'>Services</a>
+                    <a href="#projects" className='nav-link w-nav-link'>Projects</a>
+                    <a href="#blogs" className='nav-link w-nav-link'>Blogs</a>
+                    <a href="" className='nav-button w-inline-block'>
+                        <div className="nav-button-flex">
+                            <div className="nav-button-text">Book a call</div>
+                        </div>
+                        </a>
+                </nav>
+           </div>
+
+        </div>
+    );
+};
+
+export default Nav;
