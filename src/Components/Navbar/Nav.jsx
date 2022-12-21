e61912f977a8976ca85482126e000b1755afe873
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2022-12-22 01:16:44 +0600

    added font-family

--- src/Components/Navbar/Nav.jsx
+++ src/Components/Navbar/Nav.jsx
@@ -12,9 +12,10 @@ const Nav = () => {
                     <a href="#services" className='nav-link w-nav-link'>Services</a>
                     <a href="#projects" className='nav-link w-nav-link'>Projects</a>
                     <a href="#blogs" className='nav-link w-nav-link'>Blogs</a>
-                    <a href="" className='nav-button w-inline-block'>
+                    <a href="#" className='nav-button w-inline-block'>
                         <div className="nav-button-flex">
                             <div className="nav-button-text">Book a call</div>
+                            <div class="nav-button-arrow-wrapper"><img src="https://assets.website-files.com/5fef5619b640934b33c2385e/5ff019fc559a4200eda62273_Vector.svg" loading="lazy" width="13" alt=""/></div>
                         </div>
                         </a>
                 </nav>
