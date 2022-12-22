7c4592aa75bc1057feae0ce062453d5c298e90fd
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2022-12-22 13:57:22 +0600

    complete trust section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Nav from './Components/Navbar/Nav';
+import Trusted from './Components/Trusted/Trusted';
 import Home from './Pages/Home/Home';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Nav/>
       <Home/>
+      <Trusted/>
     </div>
   );
 }
