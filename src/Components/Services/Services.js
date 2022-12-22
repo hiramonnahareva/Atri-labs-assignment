a127d279de0f29da19dd992c21c74a85f39ca2e6
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2022-12-22 14:58:20 +0600

    complete service section

---
+++ src/Components/Services/Services.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import './Services.css';
+
+const Services = () => {
+    return (
+        // <div className='wrapper services'>
+        //     <div class="service-heading-and-subtext-wrapper"><div data-w-id="3b2aacaf-b523-3ac7-2e68-cb5e5cdfb99c" style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;" class="services-sub-text-wrapper"><h6>Services</h6></div><div class="service-heading-wrapper">
+        //         <h2>Design that solves problems, one product at a time.</h2>
+        //         </div>
+
+        //         </div>
+        // </div>
+        <div id="Services" class="services-section wf-section">
+            <div class="wrapper services">
+                <div class="service-heading-and-subtext-wrapper">
+                    <div class="services-sub-text-wrapper">
+                        <h6>Services</h6>
+                    </div>
+                    <div class="service-heading-wrapper">
+                        <h2>Design that solves problems, one product at a time.</h2>
+                    </div>
+                </div>
+                <div class="services-grid">
+                    <div class="service-wrapper">
+                        <div class="service-item-icon-wrapper" >
+                            <img src="https://assets.website-files.com/5fef5619b640934b33c2385e/5fef584362e74acfa6b75398_Group%2051.svg" loading="lazy" alt="" class="image contain" />
+                        </div>
+                        <div class="service-item-heading-wrapper">
+                            <h5>What I can do for you</h5>
+                        </div>
+                        <div class="service-item-paragraph-wrapper">
+                            <p class="paragraph.paragraph._16px">Faster, better products that your users love. Here's all the services I provide:</p>
+                        </div><div class="service-pointer-wrapper">
+                            <div class="service-pointer-item">
+                                <div class="service-pointer">
+                                    <div class="service-bullet-point">
+                                    </div>
+                                    <div class="service-pointer-trigger-text-wrapper">
+                                        <div class="service-pointer-trigger-text">Design Strategy</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="service-pointer-item">
+                                <div class="service-pointer">
+                                    <div class="service-bullet-point">
+                                    </div>
+                                    <div class="service-pointer-trigger-text-wrapper">
+                                        <div class="service-pointer-trigger-text">Web and Mobile App Design</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="service-pointer-item">
+                                <div class="service-pointer">
+                                    <div class="service-bullet-point"></div><div class="service-pointer-trigger-text-wrapper"><div class="service-pointer-trigger-text">Front-end Development</div></div></div></div></div></div><div class="service-wrapper"><div class="service-item-icon-wrapper" >
+                                        <img src="https://assets.website-files.com/5fef5619b640934b33c2385e/5fef5843b6409375f7c23f2b_Group%2017.svg" loading="lazy" alt="" class="image contain" />
+                                    </div>
+                        <div class="service-item-heading-wrapper" ><h5>Applications I'm fluent in</h5></div><div class="service-item-paragraph-wrapper" ><p class="paragraph _16px">Every designer needs the right tools to do the perfect job. Thankfully, I'm multilingual.</p></div><div class="service-pointer-wrapper"><div class="service-pointer-item"><div class="service-pointer" ><div class="service-bullet-point"></div><div class="service-pointer-trigger-text-wrapper"><div class="service-pointer-trigger-text">Sketch</div></div></div></div><div class="service-pointer-item"><div class="service-pointer" ><div class="service-bullet-point"></div><div class="service-pointer-trigger-text-wrapper"><div class="service-pointer-trigger-text">Webflow</div></div></div></div><div class="service-pointer-item"><div class="service-pointer">
+                            <div class="service-bullet-point">
+                                </div><div class="service-pointer-trigger-text-wrapper">
+                            <div class="service-pointer-trigger-text">
+                                <strong>Figma</strong>
+                            </div>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="service-wrapper">
+                        <div class="service-item-icon-wrapper">
+                            <img src="https://assets.website-files.com/5fef5619b640934b33c2385e/5fef58435ceaed068dde60ca_Group%2016.svg" loading="lazy" alt="" class="image contain"
+                            />
+                        </div><div class="service-item-heading-wrapper" >
+                            <h5>What you can expect</h5>
+                        </div>
+                        <div class="service-item-paragraph-wrapper" >
+                            <p class="paragraph _16px">I design products that are more than pretty. I make them shippable and usable.</p>
+                        </div>
+                        <div class="service-pointer-wrapper"><div class="service-pointer-item">
+                            <div class="service-pointer">
+                                <div class="service-bullet-point">
+                                </div>
+                                <div class="service-pointer-trigger-text-wrapper">
+                                    <div class="service-pointer-trigger-text">Clean and functional</div>
+                                </div>
+                            </div>
+                        </div>
+                            <div class="service-pointer-item">
+                                <div class="service-pointer">
+                                    <div class="service-bullet-point">
+                                    </div>
+                                    <div class="service-pointer-trigger-text-wrapper">
+                                        <div class="service-pointer-trigger-text">Device and user friendly</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="service-pointer-item">
+                                <div class="service-pointer">
+                                    <div class="service-bullet-point">
+                                    </div>
+                                    <div class="service-pointer-trigger-text-wrapper">
+                                        <div class="service-pointer-trigger-text">Efficient and maintainable</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hero from '../../Components/Hero/Hero';
+import Services from '../../Components/Services/Services';
+import Trusted from '../../Components/Trusted/Trusted';
 
 const Home = () => {
     return (
         <div>
             <Hero/>
+            <Trusted/>
+            <Services/>
         </div>
     );
 };
