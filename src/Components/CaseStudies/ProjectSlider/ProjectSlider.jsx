1bd471971ed3050b2b6d68effbf80778af5e94d5
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2022-12-28 19:48:44 +0600

    add testimonial

--- src/Components/CaseStudies/ProjectSlider/ProjectSlider.jsx
+++ src/Components/CaseStudies/ProjectSlider/ProjectSlider.jsx
@@ -5,7 +5,8 @@ const ProjectSlider = () => {
         <div className='wrapper project-slider'>
             <div className='project-slider w-slider'>
                 <div className="project-slider-mask w-slider-mask">
-                    <div className="project-slide w-slide" role="group"><div className="w-dyn-list"><div role="list" className="w-dyn-items"><div role="listitem" className="w-dyn-item"><a href="/projects/soulful-rebrand" className="project-slide-link-block w-inline-block">
+                    <div className="project-slide w-slide" role="group"><div className="w-dyn-list"><div role="list" className="w-dyn-items"><div role="listitem" className="w-dyn-item">
+                        <a href="/" className="project-slide-link-block w-inline-block">
                         <div className="project-slider-image-wrapper">
                             <img src="https://assets.website-files.com/5ff05a7bd62f16808f485280/6077dc9bcd0f7a437038f60d_image_processing20200129-19798-1k8ponz%201-min.png" loading="lazy" alt="" className="image img-hover" />
                         </div>
@@ -18,7 +19,7 @@ const ProjectSlider = () => {
                                     <img src="https://assets.website-files.com/5fef5619b640934b33c2385e/5ff019fc559a4200eda62273_Vector.svg" loading="lazy" alt="" className="view-project-arrow" /></div></div></div></a></div></div></div></div>
                                     <div className="project-slide w-slide" aria-label="2 of 5" role="group" aria-hidden="true">
                         <div className="w-dyn-list" aria-hidden="true"><div role="list" className="w-dyn-items" aria-hidden="true"><div role="listitem" className="w-dyn-item" aria-hidden="true">
-                            <a href="/projects/datadash-product-design" className="project-slide-link-block w-inline-block" tabIndex="-1" aria-hidden="true"><div className="project-slider-image-wrapper" aria-hidden="true"><img src="https://assets.website-files.com/5ff05a7bd62f16808f485280/6077dcc78ec31466630c033f_image_processing20200129-19798-1k8ponz%202-min.png" loading="lazy" alt="" className="image img-hover" aria-hidden="true" />
+                            <a href="/" className="project-slide-link-block w-inline-block" tabIndex="-1" aria-hidden="true"><div className="project-slider-image-wrapper" aria-hidden="true"><img src="https://assets.website-files.com/5ff05a7bd62f16808f485280/6077dcc78ec31466630c033f_image_processing20200129-19798-1k8ponz%202-min.png" loading="lazy" alt="" className="image img-hover" aria-hidden="true" />
                             </div>
                                 <div className="project-slider-content" aria-hidden="true">
                                     <div className="project-slider-tags-wrapper" aria-hidden="true">
@@ -33,7 +34,7 @@ const ProjectSlider = () => {
                                     </div></div></div></a></div></div></div></div>
                     <div className="project-slide w-slide" aria-label="3 of 5" role="group" aria-hidden="true">
                         <div className="w-dyn-list" aria-hidden="true"><div role="list" className="w-dyn-items" aria-hidden="true"><div role="listitem" className="w-dyn-item" aria-hidden="true">
-                            <a href="/projects/maize-website-design" className="project-slide-link-block w-inline-block" tabIndex="-1" aria-hidden="true">
+                            <a href="/" className="project-slide-link-block w-inline-block" tabIndex="-1" aria-hidden="true">
                                 <div className="project-slider-image-wrapper" aria-hidden="true">
                                     <img src="https://assets.website-files.com/5ff05a7bd62f16808f485280/6077dcd635f7f176db9fef1e_image_processing20200129-19798-1k8ponz%203-min.png" loading="lazy" alt="" className="image img-hover" aria-hidden="true" /></div><div className="project-slider-content" aria-hidden="true"><div className="project-slider-tags-wrapper" aria-hidden="true"><div className="project-slider-tag-text-wrapper" aria-hidden="true"><h6 className="project-slider-tag-text" aria-hidden="true">Web Design</h6></div></div><div className="project-slider-heading-wrapper" aria-hidden="true"><h5 className="project-slider-heading" aria-hidden="true">Maize Website Design </h5></div><div className="view-project-div" aria-hidden="true"><div className="text-block _18px" aria-hidden="true">View Project</div><div className="view-project-arrow-wrapper" aria-hidden="true"><img src="https://assets.website-files.com/5fef5619b640934b33c2385e/5ff019fc559a4200eda62273_Vector.svg" loading="lazy" alt="" className="view-project-arrow" aria-hidden="true" />
                                     </div></div></div></a></div></div></div></div>
--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Testimonials.css';
 
 const Testimonials = () => {
+    const Testimonial_data = [
+        {
+            img: 'https://assets.website-files.com/5fef5619b640934b33c2385e/604773c422dbe6174ff754e5_jake-nackos-IF9TK5Uy-KI-unsplash%2012-min.png',
+            content_text: 'This is unbelievable. After using Testiminial Generator my buisness skyrocketed!',
+            testimonial_name: 'Jennifer Musk',
+            text_block: 'Project Manager@ Microsoft',
+
+        },
+        {
+            img: 'https://assets.website-files.com/5fef5619b640934b33c2385e/604788d804dfe4ec1de7bb97_image-1-compressed.jpg',
+            content_text: "Jade helped us build a software so intuitive that it didn't need a walkthrough. He solved complex problems with brilliant design.",
+            testimonial_name: 'John Frankin',
+            text_block: 'Founder, Double Bunch',
+
+        }
+    ]
+    const [current, setCurrent] = useState(0);
+    const length = Testimonial_data.length;
+    const previous = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1);
+    };
+    const next = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1);
+    };
+    if(!Array.isArray(Testimonial_data) || Testimonial_data.length <= 0){
+        return null;
+    }
     return (
         <section>
             <div className="wrapper testimonial-headings">
@@ -13,28 +40,44 @@ const Testimonials = () => {
                 </div>
             </div>
             <div className="wrapper testimonial-down">
-                <div className="testimonial-container">
-                    <div className="testimonial-image-wrapper">
-                        <img src="https://assets.website-files.com/5fef5619b640934b33c2385e/604773c422dbe6174ff754e5_jake-nackos-IF9TK5Uy-KI-unsplash%2012-min.png" loading="lazy" alt="" className="image" />
-                    </div>
-                    <div className="testimonial-content">
-                        <div className="testimonial-quote-icon-wrapper">
-                            <img src="https://assets.website-files.com/5fef5619b640934b33c2385e/5ff6a53da27356854576b920_Group%2058.svg" loading="lazy" alt="" className="image contain" />
+                {
+                    
+                        Testimonial_data.map((item, index) => 
+                        
+                            index === current &&  <div key={index} className="testimonial-container">
+                            <div className="testimonial-image-wrapper">
+                              <img src={item.img} loading="lazy" alt="" className={index === current ? "image active-img" : ""} />
+                            </div>     
+                            <div className="testimonial-content">
+                                <div className="testimonial-quote-icon-wrapper">
+                                 <img src="https://assets.website-files.com/5fef5619b640934b33c2385e/5ff6a53da27356854576b920_Group%2058.svg" loading="lazy" alt="" className="image contain" />
+                                </div>
+                                <div className="testimonial-content-text">
+                                 <h3 className="testimonial-content-text">{item.content_text}</h3>
+                                </div>
+                                <div className="testimonial-name-and-position-wrapper">
+                                    <div className="testimonial-name-wrapper">
+                                     <div className="text-block testimonial-name">{item.testimonial_name}</div>
+                                     <div className="text-block _18px">{item.text_block}</div>
+                                        
+                                    
+                                </div>
+                                </div>
+                            </div>
+                        
                         </div>
-                        <div className="testimonial-content-text">
-                            <h3 className="testimonial-content-text">This is unbelievable. After using Testiminial Generator my buisness skyrocketed!</h3>
-                        </div>
-                        <div className="testimonial-name-and-position-wrapper">
-                            <div className="testimonial-name-wrapper">
-                                <div className="text-block testimonial-name">Jennifer Musk</div>
-                            <div className="text-block _18px">Project Manager@ Microsoft</div>
+                        
+                       
+                      
+                       
+                        )
+                    
+                }
+            
+               
                             <div className='testimonial-arrow-container'>
-                <div className="testimonial-arrow left w-slider-arrow-left" role="button" tabIndex="0" aria-controls="w-slider-mask-1" aria-label="previous slide"><div className="testimonial-arrow-icon-wrapper"><img src="https://assets.website-files.com/5fef5619b640934b33c2385e/5fef584ac8595f0e43b2b567_Vector-1.svg" loading="lazy" alt="" className="image contain"/></div></div>
-                <div className="testimonial-arrow right w-slider-arrow-right" role="button" tabIndex="0" aria-controls="w-slider-mask-1" aria-label="next slide"><div className="testimonial-arrow-icon-wrapper"><img src="https://assets.website-files.com/5fef5619b640934b33c2385e/5fef584a5ceaed4f77de60cb_Vector.svg" loading="lazy" alt="" className="image contain"/></div></div>
-                </div>
-                        </div>
-                        </div>
-                    </div>
+                <div className="testimonial-arrow left w-slider-arrow-left" onClick={previous}><div className="testimonial-arrow-icon-wrapper"><img src="https://assets.website-files.com/5fef5619b640934b33c2385e/5fef584ac8595f0e43b2b567_Vector-1.svg" loading="lazy" alt="" className="image contain"/></div></div>
+                <div className="testimonial-arrow right w-slider-arrow-right" onClick={next}><div className="testimonial-arrow-icon-wrapper"><img src="https://assets.website-files.com/5fef5619b640934b33c2385e/5fef584a5ceaed4f77de60cb_Vector.svg" loading="lazy" alt="" className="image contain"/></div></div>
                 </div>
             </div>
         </section>
